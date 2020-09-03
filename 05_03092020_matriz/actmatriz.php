ad6d4afd199e4214d213486b68a7641264bdbcc2
Saul Roldan <saulry90@gmail.com>
2020-09-03 20:59:39 +0200

    añado actmatriz

---
+++ 05_03092020_matriz/actmatriz.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>actmatriz</title>
+</head>
+<body>
+    <h1>actmatriz</h1>
+    <?php
+    echo "Números del 1 al 10:<br>";
+    for($i=1;$i<=10;$i++){
+        $numero=array(); 
+        echo "·".$numero[$i]=$i;
+    }
+    echo "<br><br>Números aleatorios:<br>";
+    $numero=array(); 
+    for($i=1;$i<=5;$i++){
+        $num_aleatorio=rand(1,100);
+        array_push($numero,$num_aleatorio);
+        echo "·".$num_aleatorio;
+        $media=(array_sum($numero))/count($numero);
+    }
+        echo "<br>La media es: ".$media;
+    ?>
+    
+</body>
+</html>
